56413ab60d2f23c88b66450939df6a88aae35d86
Will Stoddard <wstoddard89@gmail.com>
2020-09-24 16:56:59 -0700

    made start screen and cards layout

---
+++ main.js
@@ -0,0 +1,80 @@
+/* 
+PROBLEM: Create a "new game screen" with a single "new game" choice.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+
+PROBLEM: Make a game board that shows 18 cards, 9 being pairs.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+PROBLEM: Randomize cards at beginning of new game.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+PROBLEM: Make cards flip when clicked on.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+
+ 
+PROBLEM: Create "turns" or "attempts" and when the user runs out, they lose.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+PROBLEM: When the user makes a match on two consecutive clicks, leave them face up.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+PROBLEM: When the user makes two consecutive clicks that don't match, flip them
+         back over.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+PROBLEM: Make it so user is told when the win or lose.
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+
+
+PROBLEM: All screens generated from Javascript templates???
+EXAMPLE:
+DATA-STRUCTURE:
+ALGORITHM:
+CODE:
+*/
+
+window.onload = function () {
+  document.getElementById("buttonModal").onclick = function () {
+    document.getElementById("modal").style.display = "none"
+  }
+}
+// const cards = document.querySelector(".memoryGame")
+
+// function flipCard() {
+//   this.classList.toggle("flip")
+// }
+
+// cards.forEach(card => card.addEventListener('click', flipCard);
